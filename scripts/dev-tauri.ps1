45450f1db098a05faddac260ae071774ef6e11c3
Zackandre17@hotmail.com <andrefonseca172014@gmail.com>
2026-05-18 11:10:33 -0600

    Agrega script de desarrollo Tauri

---
+++ scripts/dev-tauri.ps1
@@ -0,0 +1,10 @@
+﻿$ErrorActionPreference = "Stop"
+
+$env:CARGO_TARGET_DIR = "C:\Temp\NebulosaWikiTarget"
+
+Write-Host "Nebulosa Wiki - modo desarrollo" -ForegroundColor Cyan
+Write-Host "Repositorio: D:\Aplicaciones\NebulosaWikiApp"
+Write-Host "Cargo target: $env:CARGO_TARGET_DIR"
+Write-Host ""
+
+npm run tauri dev
